9b85d0998127f6dfe5ea8cf4a24943177cea282f
Filip Iran <72192710+F1lgas@users.noreply.github.com>
2023-03-28 12:01:55 +0200

    v1.0

---
+++ test_OOP/herni_postava.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace test_OOP
+{
+    internal class herni_postava
+    {
+        String Jmeno { get; set; }
+        int PozX = 0;
+        int PozY = 0;
+        public int level = 1;
+
+        public herni_postava(string jmeno)
+        {
+            this.Jmeno = jmeno;
+        }
+
+        public int zmenaPoz(Label x, Label y, TextBox t1, TextBox t2)
+        {
+            int x2 = t1.Text();
+            int y2 = t2.Text();
+
+        }
+
+        public override string ToString()
+        {
+            return "jmeno: " + Jmeno + " X: " + PozX + " Y: " + PozY + " lvl:" + level;
+        }
+
+        public int LvlUp(int lvl)
+        {
+            lvl++;
+            return lvl;
+        }
+    }
+}
